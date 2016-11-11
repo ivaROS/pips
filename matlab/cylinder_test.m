d09edb2d27a7c52fadbfa451de49e0398349075a
justin <jssmith@gatech.edu>
2016-11-10 19:07:05 -0500

    Added top and bottom curve. The leftmost edge isn't quite correct, but otherwise looks pretty good

--- matlab/cylinder_test.m
+++ matlab/cylinder_test.m
@@ -1,7 +1,9 @@
-Xc = [-0.3,0,2]; %coordinates of robot origin in camera frame (right = x, down = y, out = z)
+Xc = [.6,.3,1.5]; %coordinates of hallucinated robot origin in camera frame (right = x, down = y, out = z)
 
 r = .18;    %radius of robot
 height = .4;%height of robot
+
+camera_height = .3;
 
 dim = [640,480];    %[width,height] of image
 
@@ -20,14 +22,19 @@ Xt_lb = [h*sin(theta_c - theta_d), Xc(2), h*cos(theta_c - theta_d)]';
 Xt_rb = [h*sin(theta_c + theta_d), Xc(2), h*cos(theta_c + theta_d)]';
 
 Xt_lt = Xt_lb + [0,-height,0]';
+Xt_rt = Xt_rb + [0,-height,0]';
 
-U_lb = Xt_lb/Xt_lb(3);
-U_rb = Xt_rb/Xt_rb(3);
-U_lt = Xt_lt/Xt_lt(3);
+
+U_lb = Xt_lb/Xt_lb(3);  %ray to bottom left point
+U_rb = Xt_rb/Xt_rb(3);  %ray to bottom right point
+U_lt = Xt_lt/Xt_lt(3);  %ray to top left point
+U_rt = Xt_rt/Xt_rt(3);  %ray to top right point
 
 p_lb = K * U_lb;
 p_rb = K * U_rb;
 p_lt = K * U_lt;
+p_rt = K * U_rt;
+
 
 p_l_int = max(1,floor(p_lb(1))); %remember to use 0 indexing in C
 p_r_int = min(ceil(p_rb(1)),dim(1));
@@ -37,17 +44,18 @@ p_t_int = max(1,floor(p_lt(2)));
 
 row_range = p_t_int:p_b_int;
 
-p_reproj = round(p_lb);
-
 simulated_image(row_range,p_l_int) = Xt_lb(3);
 simulated_image(row_range,p_r_int) = Xt_rb(3);
 
 for p_x = p_l_int+1:p_r_int-1
     
-    p_reproj(1) = p_x; %coordinates of pixel we need to calculate depth for
+    %reprojecting x pixel coordinate at center of y coordinates to ray
+    L = K\[p_x;K(2,3);1];
     
-    L = K\p_reproj;    %reprojecting pixel to ray
-
+    %%Equivalent to the below 
+    %L = K\p_reproj;    %reprojecting pixel to ray
+    %L(2) = 0;
+    
     %equations for intersection of ray and circle
     a = L(1)^2 + L(3)^2;
     b = -2*(L(1)*Xc(1) + L(3)*Xc(3));
@@ -64,12 +72,27 @@ for p_x = p_l_int+1:p_r_int-1
     t = (-b + sqrt(b^2-4*a*c))/(2*a);
     
     %get world coordinates of intersection
-    xh = L*t;
+    X_h = L*t;
+    
+    %for bottom:
+    X_hb = X_h + [0,camera_height,0]';
+    
+    %for top:
+    X_ht = X_h + [0,camera_height-height,0]';
+    
+    %project back to pixels to get y coordinate
+    U_xht = X_ht/X_ht(3);
+    p_xht = K*U_xht;
+    
+    U_xhb = X_hb/X_hb(3);
+    p_xhb = K*U_xhb;
     
     %get depth for that pixel
-    depth = xh(3);
+    depth = X_h(3);
     
-    simulated_image(row_range,p_reproj(1)) = depth;
+    row_range = max(1,floor(p_xht(2))):min(dim(2),ceil(p_xhb(2)));
+    
+    simulated_image(row_range,p_x) = depth;
 end
 
 figure(1);
