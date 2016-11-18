cfe1856201bfb0692de6b8588a205e9c7ee155c2
justin <jssmith@gatech.edu>
2016-11-18 15:41:30 -0500

    Slight alterations so that matlab script more closely resembles cpp file.

--- matlab/cylinder_test.m
+++ matlab/cylinder_test.m
@@ -1,9 +1,8 @@
 Xc = [-.3,.3,1.5]; %coordinates of hallucinated robot origin in camera frame (right = x, down = y, out = z)
 
-r = .18;    %radius of robot
-height = .4;%height of robot
-
-camera_height = .3;
+robot_radius = .178;    %radius of robot
+robot_height = .48;%height of robot
+floor_tolerance = .05;
 
 dim = [640,480];    %[width,height] of image
 
@@ -15,43 +14,52 @@ K = [575.8157348632812, 0.0, 314.5; 0.0, 575.8157348632812, 235.5; 0.0, 0.0, 1.0
 h_squared = Xc(1)^2 + Xc(3)^2;
 h = sqrt(h_squared);
 
-theta_c = atan2(Xc(1),Xc(3));
-theta_d = asin(r/h);
+left = 0;
+right = dim(1);
 
-Xt_lb = [h*sin(theta_c - theta_d), Xc(2), h*cos(theta_c - theta_d)]';
-Xt_rb = [h*sin(theta_c + theta_d), Xc(2), h*cos(theta_c + theta_d)]';
+if(h > robot_radius)
 
-Xt_lt = Xt_lb + [0,-height,0]';
-Xt_rt = Xt_rb + [0,-height,0]';
+    theta_c = atan2(Xc(1),Xc(3));
+    theta_d = asin(robot_radius/h);
 
+    Xt_lb = [h*sin(theta_c - theta_d), Xc(2)-floor_tolerance, h*cos(theta_c - theta_d)]';
+    Xt_rb = [h*sin(theta_c + theta_d), Xc(2)-floor_tolerance, h*cos(theta_c + theta_d)]';
 
-U_lb = Xt_lb/Xt_lb(3);  %ray to bottom left point
-U_rb = Xt_rb/Xt_rb(3);  %ray to bottom right point
-U_lt = Xt_lt/Xt_lt(3);  %ray to top left point
-U_rt = Xt_rt/Xt_rt(3);  %ray to top right point
-
-p_lb = K * U_lb;
-p_rb = K * U_rb;
-p_lt = K * U_lt;
-p_rt = K * U_rt;
-
-p_lb_x = min(max(1,floor(p_lb(1))),dim(1)); %remember to use 0 indexing in C
-p_lb_y = max(min(dim(2),ceil(p_lb(2))),1);
-p_lb_ind = [p_lb_x; p_lb_y];
-
-p_lt_ind = max([1;1],min(floor(p_lt(1:2)),dim'));
-
-p_rb_x = max(1,min(ceil(p_rb(1)),dim(1)));
-p_rb_y = max(min(dim(2),ceil(p_rb(2))),1);
-p_rb_ind = [p_rb_x; p_rb_y];
-
-p_rt_ind = max([1;1],min(ceil(p_rt(1:2)),dim'));
+    Xt_lt = Xt_lb + [0,-robot_height+floor_tolerance,0]';
+    Xt_rt = Xt_rb + [0,-robot_height+floor_tolerance,0]';
 
 
-simulated_image(p_lt_ind(2):p_lb_ind(2),p_lb_ind(1)) = Xt_lb(3);
-simulated_image(p_rt_ind(2):p_rb_ind(2),p_rb_ind(1)) = Xt_rb(3);
+    U_lb = Xt_lb/Xt_lb(3);  %ray to bottom left point
+    U_rb = Xt_rb/Xt_rb(3);  %ray to bottom right point
+    U_lt = Xt_lt/Xt_lt(3);  %ray to top left point
+    U_rt = Xt_rt/Xt_rt(3);  %ray to top right point
 
-for p_x = p_lb_ind(1)+1:p_rb_ind(1)-1
+    p_lb = K * U_lb;
+    p_rb = K * U_rb;
+    p_lt = K * U_lt;
+    p_rt = K * U_rt;
+
+    p_lb_x = min(max(1,floor(p_lb(1))),dim(1)); %remember to use 0 indexing in C
+    p_lb_y = max(min(dim(2),ceil(p_lb(2))),1);
+    p_lb_ind = [p_lb_x; p_lb_y];
+
+    p_lt_ind = max([1;1],min(floor(p_lt(1:2)),dim'));
+
+    p_rb_x = max(1,min(ceil(p_rb(1)),dim(1)));
+    p_rb_y = max(min(dim(2),ceil(p_rb(2))),1);
+    p_rb_ind = [p_rb_x; p_rb_y];
+
+    p_rt_ind = max([1;1],min(ceil(p_rt(1:2)),dim'));
+
+    left = p_lb_ind(1)+1;
+    right = p_rb_ind(1)-1;
+
+    simulated_image(p_lt_ind(2):p_lb_ind(2),p_lb_ind(1)) = Xt_lb(3);
+    simulated_image(p_rt_ind(2):p_rb_ind(2),p_rb_ind(1)) = Xt_rb(3);
+
+end
+
+for p_x = left:right
     
     %reprojecting x pixel coordinate at center of y coordinates to ray
     L = K\[p_x;K(2,3);1];
@@ -63,7 +71,7 @@ for p_x = p_lb_ind(1)+1:p_rb_ind(1)-1
     %equations for intersection of ray and circle
     a = L(1)^2 + L(3)^2;
     b = -2*(L(1)*Xc(1) + L(3)*Xc(3));
-    c = h_squared - r^2;
+    c = h_squared - robot_radius^2;
     
     if b^2-4*a*c < 0
         disp('Error! Complex solution!\n');
@@ -78,11 +86,14 @@ for p_x = p_lb_ind(1)+1:p_rb_ind(1)-1
     %get world coordinates of intersection
     X_h = L*t;
     
+    %set the y coordinate back to that of the base of the robot
+    X_h(2) = Xc(2);
+    
     %for bottom:
-    X_hb = X_h + [0,camera_height,0]';
+    X_hb = X_h + [0,-floor_tolerance,0]';
     
     %for top:
-    X_ht = X_h + [0,camera_height-height,0]';
+    X_ht = X_h + [0,-robot_height,0]';
     
     %project back to pixels to get y coordinate
     U_xht = X_ht/X_ht(3);
