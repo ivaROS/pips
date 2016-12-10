c4355f7f3d84093f7f7494a0d3f7499a6b6bcd12
Yipu Zhao <yipu.zhao@gatech.edu>
2016-12-09 21:15:49 -0500

    slightly modify CMakeList & cmake_module to enable autofind of Eigen3

---
+++ build/CMakeFiles/trajectory_tester.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yipuzhao/ros_workspace/package_dir/pips/src/test_trajectory.cpp" "/home/yipuzhao/ros_workspace/package_dir/pips/build/CMakeFiles/trajectory_tester.dir/src/test_trajectory.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_PACKAGE_NAME=\"pips\""
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/yipuzhao/ros_workspace/package_dir/pips/build/CMakeFiles/collision_checker.dir/DependInfo.cmake"
+  "/home/yipuzhao/ros_workspace/package_dir/pips/build/CMakeFiles/robot_models.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/opt/ros/indigo/include"
+  "/usr/include/opencv"
+  "/usr/include/eigen3"
+  ".."
+  "../include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
