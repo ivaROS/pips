41beb008b9f521e3feaf5861af75a19149fe78af
Yipu Zhao <yipu.zhao@gatech.edu>
2016-12-09 21:17:59 -0500

    slightly modify CMakeList & cmake_module to enable autofind of Eigen3

--- build/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/usr/local/include;/usr/include/eigen3".split(';') if "/usr/local/include;/usr/include/eigen3" != "" else []
-PROJECT_CATKIN_DEPENDS = "image_geometry;cv_bridge".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lcollision_checker;-lrobot_models".split(';') if "-lcollision_checker;-lrobot_models" != "" else []
-PROJECT_NAME = "pips"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "0.0.0"
