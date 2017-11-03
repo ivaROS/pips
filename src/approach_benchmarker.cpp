0a56b315c51336bda6ec59c2ce217fbe18d33596
Justin Smith <jssmith@gatech.edu>
2017-11-02 20:08:33 -0400

    Added more cases and made code neater

--- src/approach_benchmarker.cpp
+++ src/approach_benchmarker.cpp
@@ -8,11 +8,16 @@
 struct datapoint
 {
   double depth;
-  std::chrono::time_point<std::chrono::high_resolution_clock> time;
+  std::chrono::duration<double, std::milli> duration;
   uint collisions;  
  
-  datapoint(double depth, std::chrono::time_point<std::chrono::high_resolution_clock> time, uint collisions) :
-    depth(depth), time(time), collisions(collisions) {}
+  datapoint(double depth, std::chrono::time_point<std::chrono::high_resolution_clock> t1, std::chrono::time_point<std::chrono::high_resolution_clock> t2, uint collisions) :
+    depth(depth), duration(std::chrono::duration<double, std::milli>(t2-t1)), collisions(collisions) {}
+    
+  void print()
+  {
+     std::cout << "Depth = " << depth << ", collisions = " << collisions << ", in " << duration.count() << "ms" << std::endl;
+  }
 };
 
   int sequentialEval(cv::UMat roi_cl, float depth)
@@ -51,28 +56,31 @@ int run1(cv::Mat img)
   
   auto t2 = std::chrono::high_resolution_clock::now();  
   
+  std::chrono::time_point<std::chrono::high_resolution_clock> ptime = t2;
+
 
   for(float depth = .1; depth < 2.0; depth+=.01)
   {
     uint num_collisions = sequentialEval(roiIm, depth);
     auto t =  std::chrono::high_resolution_clock::now();  
 
-    datapoint d(depth,t, num_collisions);
-    res.push_back(d);
-    
-    //std::cout << "Depth = " << depth << ", collisions = " << num_collisions << std::endl;
-    
+    datapoint d(depth,ptime,t, num_collisions);
+    ptime = t;
+    res.push_back(d);    
   }
   
   std::cout << "Setup took " << std::chrono::duration<double, std::milli>(t2-t1).count() <<  "ms" << std::endl;
-
-  std::chrono::time_point<std::chrono::high_resolution_clock> ptime = t2;
   
+  std::cout << "First: ";
+  res.front().print();
+  std::cout << "Last: ";
+  res.back().print();
+  /*
   for(auto d : res)
   {
-    std::cout << "Depth = " << d.depth << ", collisions = " << d.collisions << ", in " << std::chrono::duration<double, std::milli>(d.time - ptime).count() << "ms" << std::endl;
-    ptime = d.time;
+    std::cout << "Depth = " << d.depth << ", collisions = " << d.collisions << ", in " << d.duration.count() << "ms" << std::endl;
   }
+  */
   
 }
 
@@ -80,24 +88,36 @@ int main()
 {
   cv::ocl::setUseOpenCL(true);
   int rows = 480, cols = 640;
+  
+  
+  cv::Mat img6 = cv::Mat::ones(16,32, CV_32FC1);
+  img6 *= 1.5;
+  std::cout << "32x16" << std::endl;
+  run1(img6);
+  
+  
   cv::Mat img = cv::Mat::ones(rows, cols, CV_32FC1);
   img *= 1.5;
-  
+  std::cout << "640x480" << std::endl;
   run1(img);
   
+  std::cout << "640x480" << std::endl;
   cv::Mat img2 = cv::Mat::ones(rows, cols, CV_32FC1);
   img2 *= .8;
   run1(img2);
   
   
+  std::cout << "640x479" << std::endl;
   cv::Mat img3 = cv::Mat::ones(rows-1, cols, CV_32FC1);
   img3 *= 1.5;
   run1(img3);
   
+  std::cout << "639x480" << std::endl;
   cv::Mat img4 = cv::Mat::ones(rows, cols-1, CV_32FC1);
   img4 *= 1.5;
   run1(img4);
   
+  std::cout << "640x480" << std::endl;
   cv::Mat img5 = cv::Mat::ones(rows, cols, CV_32FC1);
   img5 *= .9;
   run1(img5);
