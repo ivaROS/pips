a29303a9177aed66499a40df52335d6a4a2332d4
Justin Smith <jssmith@gatech.edu>
2017-12-12 21:50:55 -0500

    Succeeded in further speeding code (at least under 1 set of circumstances)

--- src/omp_benchmarker.cpp
+++ src/omp_benchmarker.cpp
@@ -43,10 +43,276 @@ uint16_t inner_loop(const cv::Mat& image, const T& depth)
 }
 
 
-template<typename T, uint V>
+/*
+  Vectorization factor: 8
+  Vector inside of loop cost: 2
+  Vector prologue cost: 17
+  Vector epilogue cost: 18
+  Scalar iteration cost: 3
+  Scalar outside cost: 7
+  Vector outside cost: 35
+  prologue iterations: 4
+  epilogue iterations: 4
+  Calculated minimum iters for profitability: 10
+  */
+template<typename T>
       inline
 /* Vectorizeable by gcc when using -ffast-math */
 uint16_t vect2(const cv::Mat& image, const T depth)
+{
+  int nRows = image.rows;
+  int nCols = image.cols;
+	  
+  if (image.isContinuous())
+  {
+      nCols *= nRows;
+      nRows = 1;
+  }
+  
+  int sum = 0;
+
+  for( int i = 0; i < nRows; ++i)
+  {
+  
+    const T* p = image.ptr<T>(i);
+
+    float intsum=0;
+
+    for(int j=0; j <nCols; ++j)
+    {
+
+      
+	//temp[j] = (p[j] < depth) ? 1 : 0;
+	
+	float t = 0;
+	if( p[j] < depth)
+	  t = 1;
+	
+	
+	
+	intsum += t;// temp[j];
+      
+      
+
+    }
+      sum += intsum;
+
+    
+  }
+  
+  return sum;
+
+}  
+
+
+
+
+/*
+  Vectorization factor: 4
+  Vector inside of loop cost: 3
+  Vector prologue cost: 13
+  Vector epilogue cost: 17
+  Scalar iteration cost: 4
+  Scalar outside cost: 7
+  Vector outside cost: 30
+  prologue iterations: 2
+  epilogue iterations: 2
+  Calculated minimum iters for profitability: 7
+  */
+template<typename T>
+      inline
+/* Vectorizeable by gcc when using -ffast-math */
+uint16_t vect6(const cv::Mat& image, const T depth)
+{
+  int nRows = image.rows;
+  int nCols = image.cols;
+	  
+  if (image.isContinuous())
+  {
+      nCols *= nRows;
+      nRows = 1;
+  }
+  
+  int sum = 0;
+
+  for( int i = 0; i < nRows; ++i)
+  {
+  
+    const T* p = image.ptr<T>(i);
+
+    int intsum=0;
+    int temp[nCols];
+
+    for(int j=0; j <nCols; ++j)
+    {
+
+      
+	//temp[j] = (p[j] < depth) ? 1 : 0;
+	
+	int t = 0;
+	if( p[j] < depth)
+	  t = 1;
+	
+	
+	
+	intsum += t;// temp[j];
+      
+      
+
+    }
+      sum += intsum;
+
+    
+  }
+  
+  return sum;
+
+}  
+
+/*
+  Vectorization factor: 8
+  Vector inside of loop cost: 2
+  Vector prologue cost: 17
+  Vector epilogue cost: 18
+  Scalar iteration cost: 3
+  Scalar outside cost: 7
+  Vector outside cost: 35
+  prologue iterations: 4
+  epilogue iterations: 4
+  Calculated minimum iters for profitability: 10
+  */
+template<typename T>
+      inline
+/* Vectorizeable by gcc when using -ffast-math */
+uint16_t vect5(const cv::Mat& image, const T depth)
+{
+  int nRows = image.rows;
+  int nCols = image.cols;
+	  
+  if (image.isContinuous())
+  {
+      nCols *= nRows;
+      nRows = 1;
+  }
+  
+  float sum = 0;
+
+  for( int i = 0; i < nRows; ++i)
+  {
+  
+    const T* p = image.ptr<T>(i);
+
+    float intsum=0;
+    float temp[nCols];
+
+    for(int j=0; j <nCols; ++j)
+    {
+
+      
+	//temp[j] = (p[j] < depth) ? 1 : 0;
+	
+	float t = 0;
+	if( p[j] < depth)
+	  t = 1;
+	
+	
+	
+	intsum += t;// temp[j];
+      
+      
+
+    }
+      sum += intsum;
+
+    
+  }
+  
+  return sum;
+
+}  
+
+
+
+
+/*
+  Vector inside of loop cost: 4
+  Vector prologue cost: 21
+  Vector epilogue cost: 27
+  Scalar iteration cost: 4
+  Scalar outside cost: 7
+  Vector outside cost: 48
+  prologue iterations: 4
+  epilogue iterations: 4
+  Calculated minimum iters for profitability: 11
+  */
+
+template<typename T>
+      inline
+/* Vectorizeable by gcc when using -ffast-math */
+uint16_t vect4(const cv::Mat& image, const T depth)
+{
+  int nRows = image.rows;
+  int nCols = image.cols;
+	  
+  if (image.isContinuous())
+  {
+      nCols *= nRows;
+      nRows = 1;
+  }
+  
+  uint16_t sum = 0;
+
+  for( int i = 0; i < nRows; ++i)
+  {
+  
+    const T* p = image.ptr<T>(i);
+
+    uint16_t intsum=0;
+    char temp[nCols];
+
+    for(int j=0; j <nCols; ++j)
+    {
+
+      
+	//temp[j] = (p[j] < depth) ? 1 : 0;
+	
+	char t = 0;
+	if( p[j] < depth)
+	  t = 1;
+	
+	
+	
+	intsum += t;// temp[j];
+      
+      
+
+    }
+      sum += intsum;
+
+    
+  }
+  
+  return sum;
+
+}  
+
+
+
+/*  
+  Vector inside of loop cost: 16
+  Vector prologue cost: 7
+  Vector epilogue cost: 39
+  Scalar iteration cost: 8
+  Scalar outside cost: 6
+  Vector outside cost: 46
+  prologue iterations: 0
+  epilogue iterations: 4
+  Calculated minimum iters for profitability: 6
+  */
+template<typename T, uint V>
+      inline
+/* Vectorizeable by gcc when using -ffast-math */
+uint16_t vect3(const cv::Mat& image, const T depth)
 {
   int nRows = image.rows;
   int nCols = image.cols;
@@ -68,19 +334,19 @@ uint16_t vect2(const cv::Mat& image, const T depth)
   
     const T* p = image.ptr<T>(i);
 
-    
+    int a = 0;
+
     for(int j=0; j <main_loops; ++j)
     {
       uint8_t temp[V];
       uint16_t intsum=0;
+      
 
       for(int k=0; k < V; ++k)
       {
-	int a = j*V + k;
-	//std::cout << "a: " << a << "\t";
-
 	temp[k] = (p[a] < depth) ? 1 : 0;
 	intsum += temp[k];
+	++a;
       }
       
       sum += intsum;
@@ -142,7 +408,7 @@ size_t middle_loop(const cv::Mat& img, const T& depth)
     cv::Rect rect(x,y,width,height);
     cv::Mat roi(img,rect);
     
-    
+    //Note: I should really also use 'roi' if I want to test how well does with nonaligned accesses
     uint16_t val;
     switch(A)
     {
@@ -150,10 +416,19 @@ size_t middle_loop(const cv::Mat& img, const T& depth)
 	val = inner_loop(img, depth);
 	break;
       case 1:
-	val = vect2<T, V>(img, depth);
+	val = stock(img,depth);
 	break;
       case 2:
-	val = stock(img,depth);
+	val = vect2<T>(img, depth);
+	break;
+      case 3:
+	val = vect3<T, V>(img, depth);
+	break;
+      case 4:
+	val = vect4<T>(img, depth);
+	break;
+	      case 5:
+	val = vect5<T>(img, depth);
 	break;
     }
 	
@@ -209,15 +484,15 @@ bool run_comparison(const cv::Mat& img, uint8_t num_it)
 {
       auto t1 = std::chrono::high_resolution_clock::now();
 
-      size_t val1 = parallel_outer_loop<V,2>(img,  num_it, true);
+      size_t val1 = parallel_outer_loop<V,4>(img,  num_it, false);
   
       auto t2 = std::chrono::high_resolution_clock::now();
       
-      size_t val2 = parallel_outer_loop<V,0>(img,  num_it, true);
+      size_t val2 = parallel_outer_loop<V,5>(img,  num_it, false);
 
       auto t3 = std::chrono::high_resolution_clock::now();
       
-      size_t val3 = parallel_outer_loop<V,1>(img, num_it, true);
+      size_t val3 = parallel_outer_loop<V,2>(img, num_it, false);
       
       auto t4 = std::chrono::high_resolution_clock::now();
 
@@ -228,7 +503,9 @@ bool run_comparison(const cv::Mat& img, uint8_t num_it)
 
       std::chrono::duration<double, std::milli> fp_ms3 = t4 - t3;
 
-      std::cout << "(" <<  (uint)num_it <<  "," << (uint)V << ") Stock: " << fp_ms1.count() << "ms, vectorized: " <<  fp_ms2.count()  << "ms, vect2: " << fp_ms3.count() << "ms" << std::endl << std::endl;
+      std::cout << "(" <<  (uint)num_it <<  "," << (uint)V << ") vect4: " << fp_ms1.count() << "ms, vect5: " <<  fp_ms2.count()  << "ms, vect2: " << fp_ms3.count() << "ms" << std::endl << std::endl;
+      
+      std::cout << "values: " << val1 << ", " << val2 << ", " << val3 << std::endl;
       
       if(val3 !=  val2 || val3 != val1)
       {
@@ -244,14 +521,16 @@ int main()
   
     cv::Mat img = cv::Mat::ones(rows, cols, CV_32FC1) * 1.5;
     
-    for (uint i = 1; i < 30; ++i)
+    //for (uint i = 1; i < 30; ++i)
+    uint i = 30;
     {
       bool ret = run_comparison<1>(img,  i);
-      ret = run_comparison<2>(img,  i);
+      /*ret = run_comparison<2>(img,  i);
       ret = run_comparison<4>(img,  i);
       ret = run_comparison<8>(img,  i);
       ret = run_comparison<16>(img,  i);
-
+      */
+      
     }
 
 
